25bf29020d784a89440304ab918db06599c019ca
lishmarayan <166789080+lishmarayan@users.noreply.github.com>
2025-01-13 15:07:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+   const express = require('express');
+   const mongoose = require('mongoose');
+   const cors = require('cors');
+  
+   const app = express();
+   const PORT = process.env.PORT || 5000;
+
+   app.use(cors());
+   app.use(express.json())
+
+   mongoose
+     .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+     .catch((error) => console.log(error.message));
+
+   const taskRoutes = require('./routes/taskRoutes');
+   app.use('/tasks', taskRoutes);
+   
